5201189fa7b890bea8e2f97465d17238d21f4bb4
Harsh0718-crypto <harshrai0718@gmail.com>
2024-10-02 12:56:18 +0530

    Add files via upload

---
+++ 453.py
@@ -0,0 +1,11 @@
+print ("Hello, World!")
+
+counter=5665
+ball=654
+bat=7584
+wicket=15624
+print("counter=",counter)
+print("ball=",ball)
+print("bat=",bat)
+print("wicket=",wicket)
+print("ball+bat+counter+wicet=",ball+bat+counter+wicket)
